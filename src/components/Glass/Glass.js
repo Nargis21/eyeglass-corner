d470a8e8b1ab2b1657fd96618cde3892863a271c
Nargis21 <nargisakther1933@gmail.com>
2022-03-26 21:40:13 +0600

    Update

--- src/components/Glass/Glass.js
+++ src/components/Glass/Glass.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Glass.css'
 const Glass = ({ glass }) => {
+    const { image, name, price } = glass
     return (
         <div className='glass-container'>
             <h2>Best For You</h2>
-            <img src={glass.image} alt="" />
-            <h3>{glass.name}</h3>
-            <h3>${glass.price}</h3>
+            <img src={image} alt="" />
+            <h3>{name}</h3>
+            <h3>${price}</h3>
         </div >
     );
 };
--- src/components/Glasses/Glasses.js
+++ src/components/Glasses/Glasses.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Glasses.css'
 const Glasses = ({ details }) => {
+    const { image, name } = details
     return (
         <div className='glasses-container'>
-            <img src={details.image} alt="" />
-            <h4>{details.name}</h4>
+            <img src={image} alt="" />
+            <h4>{name}</h4>
         </div>
     );
 };
