7bb712545f296f20956ca5f2dd9dc0c81a9e8abe
Nargis21 <nargisakther1933@gmail.com>
2022-03-26 09:23:37 +0600

    First commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <div className="products-container">
+                <h1>Here is the products</h1>
+            </div>
+            <div className="cart-container">
+                <h1>Order Summary</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
