7253a6c536190cef102bdc3aea87129b25363af6
Nargis21 <nargisakther1933@gmail.com>
2022-03-26 16:35:41 +0600

    Update

--- src/components/Glass/Glass.js
+++ src/components/Glass/Glass.js
@@ -4,6 +4,7 @@ const Glass = ({ glass }) => {
     return (
         <div className='glass-container'>
             <h2>Best For You</h2>
+            <img src={glass.image} alt="" />
             <h3>{glass.name}</h3>
             <h3>${glass.price}</h3>
         </div >
